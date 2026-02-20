42ec151f70a62874c62ffe6259a3116e5170247a
aishwaryava1917402-byte <aishwaryava1917402@gmail.com>
2026-02-20 09:45:35 +0000

    auto-commit for 8e782c3e-a5dc-42d4-98ae-6adf63c34603

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Assessment from "./pages/Assessment";
 import Results from "./pages/Results";
+import ResultsMBTI from "./pages/ResultsMBTI";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/assessment/:assessmentId" element={<Assessment />} />
           <Route path="/results/:assessmentId" element={<Results />} />
+          <Route path="/results-mbti/:assessmentId" element={<ResultsMBTI />} />
         </Routes>
       </BrowserRouter>
     </div>
